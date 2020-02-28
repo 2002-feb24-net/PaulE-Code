926846894328878d43db5ed57d6112d51be8fb4b
Paul <pauldedwards95@gmail.com>
2020-02-28 09:02:33 -0600

    midway

---
+++ Day5MorningWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day5MorningWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a multi-word phrase. For example, 'computer processing unit'.");
+            Acronize(Console.ReadLine());
+        }
+
+        static string Acronize(string x)
+        {
+            string[] MyWordList = x.Split(" ");
+            for (int i; i<MyWordList.Length;i++)
+            {
+
+            }
+            return;
+        }
+    }
+}
