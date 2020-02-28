48c5967d0916989d5a2b8337ed3ac84d28da72e2
Paul <pauldedwards95@gmail.com>
2020-02-28 09:07:22 -0600

    alpha

--- Day5MorningWork/Program.cs
+++ Day5MorningWork/Program.cs
@@ -7,17 +7,21 @@ namespace Day5MorningWork
         static void Main(string[] args)
         {
             Console.WriteLine("Please enter a multi-word phrase. For example, 'computer processing unit'.");
-            Acronize(Console.ReadLine());
+            Console.WriteLine(Acronize(Console.ReadLine()));
         }
 
         static string Acronize(string x)
         {
             string[] MyWordList = x.Split(" ");
-            for (int i; i<MyWordList.Length;i++)
-            {
+            string result = "";
 
+            for (int i=0; i<MyWordList.Length;i++)
+            {
+                result = result + MyWordList[i].Substring(0,0);
+                MyWordList[i].TrimStart();
             }
-            return;
+            Console.WriteLine(result);
+            return result;
         }
     }
 }
