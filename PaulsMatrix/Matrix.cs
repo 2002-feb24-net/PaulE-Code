afb31037e8c0c211fb269c53431747dd0d3766e5
Paul <pauldedwards95@gmail.com>
2020-03-04 13:54:45 -0600

    finished matrix assignment

---
+++ PaulsMatrix/Matrix.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+
+namespace PaulsMatrix
+{
+    class Matrix
+    {
+        public int[,] MyMatrix;
+
+        public Matrix(int[] a,int[] b)
+        {
+            MyMatrix = new int[2,2];
+            for (int i = 0;i<a.Length;i++)
+            {
+                this.MyMatrix[0,i] = a[i];
+                this.MyMatrix[1,i] = b[i];
+            }
+
+        }
+
+        public int Get (int a, int b)
+        {
+            return this.MyMatrix[a,b];
+        }
+
+        public void Add(Matrix x)
+        {
+            int total1 = this.MyMatrix[0,0] + x.Get(0,0);
+            int total2 = this.MyMatrix[0,1] + x.Get(0,1);
+            int total3 = this.MyMatrix[1,0] + x.Get(1,0);
+            int total4 = this.MyMatrix[1,1] + x.Get(1,1);
+            Console.WriteLine("[{0} , {1}]", total1, total2);
+            Console.WriteLine("[{0} , {1}]", total3, total4);
+        }
+
+        public void Subtract(Matrix x)
+        {
+            int total1 = this.MyMatrix[0,0] - x.Get(0,0);
+            int total2 = this.MyMatrix[0,1] - x.Get(0,1);
+            int total3 = this.MyMatrix[1,0] - x.Get(1,0);
+            int total4 = this.MyMatrix[1,1] - x.Get(1,1);
+            Console.WriteLine("[{0} , {1}]", total1, total2);
+            Console.WriteLine("[{0} , {1}]", total3, total4);
+        }
+
+        public void Multiply(Matrix x)
+        {
+            int total1 = this.MyMatrix[0,0] * x.Get(0,0);
+            int total2 = this.MyMatrix[0,1] * x.Get(0,1);
+            int total3 = this.MyMatrix[1,0] * x.Get(1,0);
+            int total4 = this.MyMatrix[1,1] * x.Get(1,1);
+            Console.WriteLine("[{0} , {1}]", total1, total2);
+            Console.WriteLine("[{0} , {1}]", total3, total4);
+        }
+
+        public void Reverse()
+        {
+            int total1 = this.MyMatrix[0,0]; 
+            int total2 = this.MyMatrix[0,1]; 
+            int total3 = this.MyMatrix[1,0];
+            int total4 = this.MyMatrix[1,1];
+            this.MyMatrix[0,0] = total4;
+            this.MyMatrix[0,1] = total3;
+            this.MyMatrix[1,0] = total2;
+            this.MyMatrix[1,1] = total1;
+            Console.WriteLine("[{0} , {1}]", MyMatrix[0,0], MyMatrix[0,1]);
+            Console.WriteLine("[{0} , {1}]", MyMatrix[1,0], MyMatrix[1,1]);
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0;i<2;i++)
+            {
+                for (int q=0;q<2;q++)
+                {
+                    Console.WriteLine("Array {0}: {1}",i, MyMatrix[i,q]);
+                }
+            }
+        }
+    }
+}
---
+++ PaulsMatrix/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PaulsMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(Matrix 1) Please input your first two numbers, with a comma seperating them. eg: 1,2");
+            int [] a = Question1();
+            Console.WriteLine("(Matrix 1) Please input your second two numbers, with a comma seperating them. eg: 1,2");
+            int [] b = Question1();
+
+            Matrix Matrix1 = new Matrix(a,b);
+            Matrix1.PrintMatrix();
+
+            Console.WriteLine("(Matrix 2) Please input your first two numbers, with a comma seperating them. eg: 1,2");
+            a = Question1();
+            Console.WriteLine("(Matrix 2) Please input your second two numbers, with a comma seperating them. eg: 1,2");
+            b = Question1();
+            Matrix Matrix2 = new Matrix(a,b);
+            Matrix2.PrintMatrix();
+
+            Console.WriteLine("What would you like to do with these two matrixes?");
+            Console.WriteLine("OPTIONS: ADD, SUBTRACT, MULTIPLY, REVERSE");
+            Question2(Matrix1,Matrix2);
+        }
+
+        public static int[] Question1()
+        {
+            string x = Console.ReadLine();
+            string[] y = x.Split(",");
+            int[] z = new int[y.Length];
+            for (int i =0;i<y.Length;i++)
+            {
+                z[i] = int.Parse(y[i]);
+            }
+
+            return z;
+        }
+
+        public static void Question2(Matrix a, Matrix b)
+        {
+            Matrix Matrix1 = a;
+            Matrix Matrix2 = b;
+            char x = Convert.ToChar((Console.ReadLine()).Substring(0,1).ToUpper());
+            switch (x)
+            {
+                case 'A':
+                Matrix1.Add(Matrix2);
+                break;
+
+                case 'S':
+                Matrix1.Subtract(Matrix2);
+                break;
+
+                case 'M':
+                Matrix1.Multiply(Matrix2);
+                break;
+
+                case 'R':
+                Matrix1.Reverse();
+                Matrix2.Reverse();
+                break;
+            }
+        }
+    }
+}
