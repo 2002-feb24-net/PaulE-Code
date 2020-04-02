c536e67c966a63cc3545cc144c3f871fefc483a3
Paul Edwards <pauldedwards95@gmail.com>
2020-04-02 11:49:55 -0500

    pp merged

---
+++ StudentApp/StudentApp.Data/SecretConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentApp.Data
+{
+    internal class SecretConfiguration
+    {
+        internal const string ConnectionString = "Server=tcp:2020-training-edwards.database.windows.net,1433; Initial Catalog = PersonDB; Persist Security Info=False; User ID = pedwards95; Password=Smokeysmokey1;MultipleActiveResultSets=False; Encrypt=True; TrustServerCertificate=False;Connection Timeout = 30;";
+    }
+}
