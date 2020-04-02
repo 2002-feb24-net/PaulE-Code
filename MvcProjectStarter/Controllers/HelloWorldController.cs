f8b310e8d6164d881ab5d70fcff0d1df41d335ca
Paul Edwards <pauldedwards95@gmail.com>
2020-04-02 11:44:53 -0500

    powerpoint

---
+++ MvcProjectStarter/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcProjectStarter.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public int TotalRequestCount { get; set; } = 0;
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            TotalRequestCount++;
+            return View();
+        }
+
+        public IActionResult Welcome(string name)
+        {
+            TotalRequestCount++;
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = TotalRequestCount;
+
+            return View();
+        }
+    }
+}
---
+++ MvcProjectStarter/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcProjectStarter.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcProjectStarter.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcSongContext(serviceProvider.GetRequiredService<DbContextOptions<MvcSongContext>>()))
+            {
+                if(context.Song.Any())
+                {
+                    return; //if there are already songs in the db, dont need to do anything
+                }
+
+                //else...
+                context.Song.AddRange(
+                    new Song
+                    {
+                        title = "Don't Stop Believin'",
+                        genre = "Rock",
+                        artist = "Journey",
+                        album = "Escape",
+                        ReleaseDate = DateTime.Parse("1981-3-13")
+                    },
+
+                    new Song
+                    {
+                        title = "Thriller",
+                        genre = "Rock",
+                        artist = "Micheal J",
+                        album = "Thriller",
+                        ReleaseDate = DateTime.Parse("1981-4-14")
+                    },
+
+                    new Song
+                    {
+                        title = "Eye of the Tiger",
+                        genre = "Rock",
+                        artist = "Survivor",
+                        album = "Unknown",
+                        ReleaseDate = DateTime.Parse("1985-3-13")
+                    }
+                    );
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
